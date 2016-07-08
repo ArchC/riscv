37e2038e6b6536e6385948d1ddb5f5cd5ff67766
Pavani14 <pavani13147@iiitd.ac.in>
2016-07-08 12:45:39 +0530

    Formatted according to clang-format

--- riscv_gdb_funcs.cpp
+++ riscv_gdb_funcs.cpp
@@ -12,29 +12,22 @@
 
 using namespace riscv_parms;
 
-int riscv::nRegs(void){
-	return 33;
+int riscv::nRegs(void) { return 33; }
+
+ac_word riscv::reg_read() {
+  if ((reg >= 0) && (reg < 32))
+    return RB.read(reg);
+  else if (reg == 32)
+    return ac_pc;
 }
 
-ac_word riscv::reg_read(){
-	if((reg>=0) && (reg<32))
-		return RB.read(reg);
-	else if(reg == 32)
-		return ac_pc;
+ac_word riscv::reg_write() {
+  if ((reg >= 0) && (reg < 32))
+    RB.write(reg, value);
+  else if (reg == 32)
+    ac_pc = value;
 }
 
-ac_word riscv::reg_write(){
-	if((reg>=0) && (reg<32))
-		RB.write(reg, value);
-	else if(reg == 32)
-		ac_pc = value;
-}
+unsigned char riscv::mem_read() { return IM->read_byte(address); }
 
-
-unsigned char riscv::mem_read(){
-	return IM->read_byte(address);
-}
-
-void riscv::mem_write(){
-	IM->write_byte(adress, byte);
-}
+void riscv::mem_write() { IM->write_byte(adress, byte); }
