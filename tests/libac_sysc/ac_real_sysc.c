2f82faa9efce8d80fe31355739bebbdacb1618b2
Pavani14 <pavani13147@iiitd.ac.in>
2016-08-21 02:39:59 +0530

    Platform for creating elf

---
+++ tests/libac_sysc/ac_real_sysc.c
@@ -0,0 +1,44 @@
+/**
+ * @file      ac_real_sysc.c
+ * @author    The ArchC Team
+ *            http://www.archc.org/
+ *
+ *            Computer Systems Laboratory (LSC)
+ *            IC-UNICAMP
+ *            http://www.lsc.ic.unicamp.br/
+ *
+ * @version   1.0
+ * @date      Mon, 19 Jun 2006 15:33:20 -0300
+ *
+ * @brief     
+ *
+ * @attention Copyright (C) 2002-2006 --- The ArchC Team
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+/* some syscalls that trap to simulator functions */
+
+#ifdef ARM
+#include "ac_arm.h" 
+#endif
+
+#define AC_SYSC(NAME,LOCATION) \
+int NAME() { \
+  goto *LOCATION; \
+}
+
+#include "ac_syscall.def"
---
+++ tests/libac_sysc/gettod.c
@@ -0,0 +1,46 @@
+/**
+ * @file      gettod.c
+ * @author    Marcus Bartholomeu
+ *
+ *            The ArchC Team
+ *            http://www.archc.org/
+ *
+ *            Computer Systems Laboratory (LSC)
+ *            IC-UNICAMP
+ *            http://www.lsc.ic.unicamp.br/
+ *
+ * @version   1.0
+ * @date      Mon, 19 Jun 2006 15:33:20 -0300
+ *
+ * @brief     Weak version of gettimeofday function (based in time()).
+ *
+ * @attention Copyright (C) 2002-2006 --- The ArchC Team
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <sys/time.h>
+#include <errno.h>
+
+//int
+//gettimeofday( struct timeval  *ptimeval,
+//	      struct timezone *ptimezone)
+//{
+//  int t = time(0);
+//  ptimeval->tv_sec = t;
+//  ptimeval->tv_usec = 0;
+//  return 0;
+//}
